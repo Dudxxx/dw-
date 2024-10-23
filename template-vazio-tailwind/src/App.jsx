8f56a9068d435525688549467fc1d716d4cbc2a0
Dudxxx <130068099+Dudxxx@users.noreply.github.com>
2024-10-23 11:30:26 -0300

    .

---
+++ template-vazio-tailwind/src/App.jsx
@@ -0,0 +1,14 @@
+import { TemaProvider } from "./context/TemaContext"
+import { Home } from "./pages/Home"
+
+function App() {
+  
+
+  return (
+    <TemaProvider>
+      <Home/>
+    </TemaProvider>
+  )
+}
+
+export default App
---
+++ template-vazio-tailwind/src/context/TemaContext.jsx
@@ -0,0 +1,20 @@
+// TemaContext.js
+import React, { createContext, useState } from 'react';
+
+// Criação do contexto para o tema
+export const TemaContext = createContext();
+
+
+export function TemaProvider({ children }) {
+  const [tema, setTema] = useState('claro');
+
+  const alternarTema = () => {
+    setTema( tema === 'claro' ? 'escuro' : 'claro');
+  };
+
+  return (
+    <TemaContext.Provider value={{ tema, alternarTema }}>
+      {children}
+    </TemaContext.Provider>
+  );
+}
